1d46678b8c56f72de085d156572fb7f1f5c60a28
Vitaliy <vitaliy.markitanov@trader.ca>
2026-03-09 16:38:14 -0400

    Add notifications feature

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const config = require('./config.json');
 const { loginUser, logoutUser } = require('./login');
 const { renderDashboard, refreshDashboard } = require('./dashboard');
+const { getNotifications, markAsRead } = require('./notifications');
 
 function startApp() {
   console.log(`Starting ${config.appName} v${config.version}`);
-  console.log('Modules loaded: login, dashboard');
+  console.log('Modules loaded: login, dashboard, notifications');
 }
 
 startApp();
 
-module.exports = { startApp, loginUser, logoutUser, renderDashboard, refreshDashboard };
+module.exports = { startApp, loginUser, logoutUser, renderDashboard, refreshDashboard, getNotifications, markAsRead };
